f1a79d91389c9a7e92f43582e56b67fd142c3309
Guillaume-G9 <ggonthier@live.fr>
2022-03-02 11:14:33 +0100

    fixes

--- easyCalc.js
+++ easyCalc.js
@@ -1,14 +1,16 @@
 let result = document.querySelector('#final');
-result.textContent = 0;
 let numbers = [];
 let totalResult = [];
-let numberOne = []
+let numberOne = [];
+
+result.textContent = 0;
 
 export function display() {
     result.textContent = 0;
     document.querySelectorAll('.number').forEach(input => input.addEventListener('click', (e) => {
         numbers.push(Number(e.target.textContent));
         result.textContent = numbers.join('')
+        console.log('entry', result.textContent)
 
     }))
 }
@@ -32,33 +34,41 @@ export function erase() {
         }
     })
 }
-let operator;
 
 export function calculation(numberOne, numberTwo) {
+    let operator ;
     document.querySelectorAll('.operator').forEach(input => input.addEventListener('click', (e) => {
         numberOne = numbers.join('');
         numbers = [];
         result.textContent = 0;
+        operator = e.target.id
+        
+        if (!operator === add || subtract || multiply || divide) {
+            numberTwo = numbers.join('');
+            console.log('num2',numberTwo)
+        } else {
+        } 
 
         switch (e.target.id) {
             case 'add':
-                totalResult = Number(numberOne) + Number(totalResult);
-                console.log(totalResult);
+                totalResult = Number(numberOne) + Number(numberTwo);
+                console.log('num1', numberOne)
                 break;
             case 'subtract':
-                totalResult = Number(numberOne) - Number(totalResult);
+                totalResult = Number(numberOne) - Number(numberTwo);
                 break;
             case 'multiply':
-                totalResult = Number(numberOne) * Number(totalResult);
+                totalResult = Number(numberOne) * Number(numberTwo);
                 break;
             case 'divide':
-                totalResult = Number(numberOne) / Number(totalResult);
+                totalResult = Number(numberOne) / Number(numberTwo);
                 break;
             default:
         }
     }))
-
+                        
     document.querySelector("#equal").addEventListener('click', () => {
-        result.textContent = totalResult;
+    result.textContent = totalResult;
+    console.log('equals', result.textContent)
     })
 } 
